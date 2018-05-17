8c1593ad8aa4457730eeef5ee87d0b4b6f15a149
Nancy Zitle Juarez <nancy@MacBook-Pro-de-Nancy.local>
2018-05-16 23:21:04 -0500

    import redux and enable the dev tool of redux/chrome

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const $form = document.getElementById('form');
 $form.addEventListener('submit',handleSubmit);
 
@@ -7,3 +9,20 @@ function handleSubmit(e){
   const title = data.get('title');
   console.log(title);
 }
+const initialState = [
+  {
+    "title":"despacito"
+  },
+  {
+    "title":"hechame la culpa"
+  },
+  {
+    "title":"aquí estoy yo"
+  }
+]
+// asi creamos nuestro store 
+const store = createStore(
+  (state) => state,   // reducer
+  initialState, // preloadstate
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //enhancer
+);
