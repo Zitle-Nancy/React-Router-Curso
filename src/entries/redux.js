35b83a3e13c55f7593a147383dcd9cd070d35b7b
Nancy Zitle Juarez <nancy@MacBook-Pro-de-Nancy.local>
2018-05-22 00:57:40 -0500

    add the last phase of the cicle of redux, store.subscribe

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -27,20 +27,40 @@ const initialState = [
   {
     "title":"aquí estoy yo"
   }
-]
+];
+
+const reducer = function(state,action){
+  switch(action.type){
+    case 'ADD_SONG':
+    return [...state, action.playload]
+    default:
+    return state
+  }
+}
 // asi creamos nuestro store 
 const store = createStore(
-  (state) => state,   // reducer
+  // (state) => state,   // reducer
+  reducer, // la forma correcta de poner reducer
   initialState, // preloadstate
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //enhancer
 );
 // print the result in the body 
-const $container = document.getElementById('container');
-const playList = store.getState();
-playList.forEach( (item)=>{
-  // console.log(item, 'elementos');
-  const p = document.createElement('p');
-  p.innerText = item.title;
-  $container.appendChild(p);
-})
+function render(){
+  const $container = document.getElementById('container');
+  const playList = store.getState();
+  $container.innerHTML = " "; // limpia la lista que se ha imprimido y por ende solo te va a mostrar la nueva cancion que agregue
+  playList.forEach( (item)=>{
+    // console.log(item, 'elementos');
+    const p = document.createElement('p');
+    p.innerText = item.title;
+    $container.appendChild(p);
+  });
+}
+
+function handleChange(){
+  render();
+}
+
+// con esto actualizamos nuestra aplicación
+store.subscribe(handleChange);
 // console.log(store.getState());
